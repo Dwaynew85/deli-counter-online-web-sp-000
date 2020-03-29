efaeddc7fa2a9df0bee93a4feade625d1ddcd83f
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-03-29 04:14:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,17 @@
 katz_deli = []
 
 def line(katz_deli)
-  customer_num = katz_deli.length + 1
-  if katz_deli.length > 0
-    puts "You are number #{customer_num} in line."
+  if katz_deli.length == 1
+    customer_num = katz_deli.length - 1
+    puts "The line is currently: #{katz_deli.length}. #{katz_deli[0]}"
+  elsif katz_deli.length > 1
+    katz_deli.each do |customers|
+      customer_num = katz_deli.index(customers) + 1
+      puts "The line is currently: #{customer_num}. #{customers}."
+    end
   else
     puts "The line is currently empty."
   end
 end
+
+    
